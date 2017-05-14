b13315ce250a9c3f8c4ee726201f1dca83b39363
Andreas Schmidt <a.schmidt@siberas.de>
2017-05-14 13:33:25 +0200

    fixed crash on missing xterm

--- plugins/sqlmap/gui/main.rb
+++ plugins/sqlmap/gui/main.rb
@@ -1,20 +1,21 @@
 # @private 
-module Watobo#:nodoc: all
+module Watobo #:nodoc: all
   module Plugin
     class Sqlmap
       class SettingsTabBook < FXTabBook
         attr :general
+
         def initialize(owner)
           #@tab = FXTabBook.new(self, nil, 0, LAYOUT_FILL_X|LAYOUT_FILL_Y|LAYOUT_RIGHT)
           super(owner, nil, 0, LAYOUT_FILL_X|LAYOUT_FILL_Y|LAYOUT_RIGHT)
           FXTabItem.new(self, "General", nil)
           @general = OptionsFrame.new(self, :opts => LAYOUT_FILL_X|LAYOUT_FILL_Y|FRAME_RAISED)
 
-        #   FXTabItem.new(self, "Advanced", nil)
-        #   frame = FXVerticalFrame.new(self, :opts => LAYOUT_FILL_X|LAYOUT_FILL_Y|FRAME_RAISED)
-        #   FXTabItem.new(self, "Log", nil)
-        #   frame = FXVerticalFrame.new(self, :opts => LAYOUT_FILL_X|LAYOUT_FILL_Y|FRAME_THICK|FRAME_RAISED)
-        #   @log_viewer = Watobo::Gui::LogViewer.new(frame, :append, :opts => LAYOUT_FILL_X|LAYOUT_FILL_Y|FRAME_SUNKEN)
+          #   FXTabItem.new(self, "Advanced", nil)
+          #   frame = FXVerticalFrame.new(self, :opts => LAYOUT_FILL_X|LAYOUT_FILL_Y|FRAME_RAISED)
+          #   FXTabItem.new(self, "Log", nil)
+          #   frame = FXVerticalFrame.new(self, :opts => LAYOUT_FILL_X|LAYOUT_FILL_Y|FRAME_THICK|FRAME_RAISED)
+          #   @log_viewer = Watobo::Gui::LogViewer.new(frame, :append, :opts => LAYOUT_FILL_X|LAYOUT_FILL_Y|FRAME_SUNKEN)
         end
       end
 
@@ -28,12 +29,21 @@ module Watobo#:nodoc: all
 
         end
 
+        def check_linux_terminals
+          return nil
+          ['gnome-terminal', 'xterm'].each do |t|
+            xterm_bin = "/usr/bin/#{t}"
+            return xterm_bin if File.exist? xterm_bin
+          end
+          nil
+        end
+
         def initialize(owner, project=nil, chat=nil)
-          super(owner, "SQLMap", project, :opts => DECOR_ALL, :width=>800, :height=>600)
+          super(owner, "SQLMap", project, :opts => DECOR_ALL, :width => 800, :height => 600)
           @plugin_name = "SQLMap"
 
           FXMAPFUNC(SEL_COMMAND, ID_ACCEPT, :onAccept)
-          
+
           main = FXVerticalFrame.new(self, :opts => LAYOUT_FILL_X|LAYOUT_FILL_Y)
           matrix = FXMatrix.new(main, 3, :opts => MATRIX_BY_COLUMNS|LAYOUT_FILL_X)
           FXLabel.new(matrix, "sqlmap path:")
@@ -44,31 +54,31 @@ module Watobo#:nodoc: all
           bin_path ="not defined" if bin_path.empty?
           @binary_path_txt.text = bin_path
 
-          @change_btn = FXButton.new(matrix, "...", :opts => BUTTON_DEFAULT|BUTTON_NORMAL )
+          @change_btn = FXButton.new(matrix, "...", :opts => BUTTON_DEFAULT|BUTTON_NORMAL)
           @change_btn.enable
 
-          @change_btn.connect(SEL_COMMAND){
+          @change_btn.connect(SEL_COMMAND) {
             open_path = nil
             unless @binary_path_txt.text.empty?
               dir_name = File.dirname(@binary_path_txt.text)
               unless dir_name.empty?
-              open_path = dir_name unless File.exist? dir_name
+                open_path = dir_name unless File.exist? dir_name
               end
             end
             bin_path_old = @binary_path_txt.text
             bin_path = FXFileDialog.getOpenFilename(self, "Select SQLmap Path", open_path)
             unless bin_path.empty?
               @binary_path_txt.text = bin_path
-            else              
+            else
               @binary_path_txt.text = bin_path_old
-              
+
             end
             if File.exist? @binary_path_txt.text
-            Watobo::Plugin::Sqlmap.set_binary_path bin_path
+              Watobo::Plugin::Sqlmap.set_binary_path bin_path
               @accept_btn.enable
             else
               Watobo::Plugin::Sqlmap.set_binary_path ''
-              @accept_btn.disable 
+              @accept_btn.disable
             end
           }
 
@@ -78,55 +88,54 @@ module Watobo#:nodoc: all
           @output_path_txt = FXTextField.new(matrix, 60, nil, 0, :opts => TEXTFIELD_NORMAL|LAYOUT_SIDE_RIGHT|LAYOUT_FILL_X)
           @output_path_txt.text = Watobo::Plugin::Sqlmap.tmp_dir
 
-          @output_path_btn = FXButton.new(matrix, "...", :opts => BUTTON_DEFAULT|BUTTON_NORMAL )
+          @output_path_btn = FXButton.new(matrix, "...", :opts => BUTTON_DEFAULT|BUTTON_NORMAL)
           @output_path_btn.enable
 
-          @output_path_btn.connect(SEL_COMMAND){
+          @output_path_btn.connect(SEL_COMMAND) {
             output_path = FXFileDialog.getOpenDirectory(self, "Select Temp Directory", Watobo::Plugin::Sqlmap.tmp_dir)
 
             #puts ">> #{output_path}"
             unless output_path.empty?
-            @output_path_txt.text = output_path
-            Watobo::Plugin::Sqlmap.set_tmp_dir output_path
+              @output_path_txt.text = output_path
+              Watobo::Plugin::Sqlmap.set_tmp_dir output_path
             end
           }
 
           @settings_tab = SettingsTabBook.new(main)
 
           unless chat.nil?
-          @settings_tab.general.request = chat.request
+            @settings_tab.general.request = chat.request
           end
 
           # @log_viewer = @settings_tabbook.log_viewer
 
           buttons = FXHorizontalFrame.new(main, :opts => LAYOUT_SIDE_BOTTOM|LAYOUT_FILL_X|PACK_UNIFORM_WIDTH,
-          :padLeft => 40, :padRight => 40, :padTop => 20, :padBottom => 20)
+                                          :padLeft => 40, :padRight => 40, :padTop => 20, :padBottom => 20)
           @accept_btn = FXButton.new(buttons, "&Start", nil, self, ID_ACCEPT,
-          FRAME_RAISED|FRAME_THICK|LAYOUT_RIGHT|LAYOUT_CENTER_Y)
+                                     FRAME_RAISED|FRAME_THICK|LAYOUT_RIGHT|LAYOUT_CENTER_Y)
           @accept_btn.disable
           @accept_btn.enable unless Watobo::Plugin::Sqlmap.binary_path.empty?
           # Cancel
           FXButton.new(buttons, "&Cancel", nil, self, ID_CANCEL,
-          FRAME_RAISED|FRAME_THICK|LAYOUT_RIGHT|LAYOUT_CENTER_Y)
-        # Configuration Categories
-        # =
-        # Request
-        # Optimization
-        # Detection
-        # Techniques
-        # Fingerprint
-        # Enumeration
-        
-        
-            
-            @accept_btn.disable if @settings_tab.general.request.empty?
-            @settings_tab.general.subscribe(:request_changed){
-              if @settings_tab.general.request.empty?
-                @accept_btn.disable 
-              else
-                @accept_btn.enable
-              end
-            }
+                       FRAME_RAISED|FRAME_THICK|LAYOUT_RIGHT|LAYOUT_CENTER_Y)
+          # Configuration Categories
+          # =
+          # Request
+          # Optimization
+          # Detection
+          # Techniques
+          # Fingerprint
+          # Enumeration
+
+
+          @accept_btn.disable if @settings_tab.general.request.empty?
+          @settings_tab.general.subscribe(:request_changed) {
+            if @settings_tab.general.request.empty?
+              @accept_btn.disable
+            else
+              @accept_btn.enable
+            end
+          }
         end
 
         private
@@ -135,7 +144,7 @@ module Watobo#:nodoc: all
           fname = "sqlmap_" + Time.now.to_i.to_s + ".req"
           begin
             file = File.join(@output_path_txt.text, fname)
-            File.open(file, "w"){ |fh|
+            File.open(file, "w") { |fh|
               fh.puts @settings_tab.general.request
             }
             return file
@@ -161,41 +170,46 @@ module Watobo#:nodoc: all
 
         def linux_command(file)
           # /usr/bin/xterm -hold -e "script -c \"ls -alh\" test234.out"
-          xterm_bin = "/usr/bin/xterm"
-          return false unless File.exist? xterm_bin
-          command = "cd #{@output_path_txt.text} && #{xterm_bin} -hold -e \""
+          term_bin = check_linux_terminals
+          return nil if term_bin.nil?
+
+          command = "cd #{@output_path_txt.text} && #{term_bin} "
+          command << '-hold ' unless term_bin.match(/xterm/).nil?
+          command << '-e "'
           script_cmd = "#{sqlmap_command(file)}"
           command << script_cmd
           command << '"'
-          puts command
           command
         end
-        
-        def win_command(file)
-        # start "sqlmap" /WAIT /D c:\tools dir
-          command = ""
 
-          out_file = file.gsub(/\.req/, ".out")
+        def win_command(file)
+          # start "sqlmap" /WAIT /D c:\tools dir
+          command = ''
           start_path = "#{@output_path_txt.text}"
-          start_path.gsub!(/\//,'\\')
-          
+          start_path.gsub!(/\//, '\\')
+
           script_cmd = "start \"SQLmap\" /D #{start_path} /WAIT cmd.exe /k \"#{sqlmap_command(file)}\""
           command << script_cmd
           command << '"'
-          puts command
           command
         end
 
         def run_sqlmap(file)
           command = case RUBY_PLATFORM
-          when /linux|bsd|solaris|hpux|darwin/
-            linux_command file
-          when /mswin|mingw|bccwin/
-            win_command file
+                      when /linux|bsd|solaris|hpux|darwin/
+                        linux_command file
+                      when /mswin|mingw|bccwin/
+                        win_command file
+                    end
+
+          unless command.nil? or command.strip.empty?
+            Thread.new(command) { |cmd|
+              system(cmd)
+            }
+          else
+            FXMessageBox.information(self,MBOX_OK,"Xterm is not installed", "Xterm is required to run external commands. Please install 'xterm', e.g. 'apt-get install xterm'")
           end
-          Thread.new(command){ |cmd|
-            system(cmd)
-          }
+
 
         end
 
@@ -207,9 +221,9 @@ module Watobo#:nodoc: all
           rf = create_request_file
           puts "Start SQLMap with file #{rf}"
           run_sqlmap(rf)
-        #getApp().stopModal(self, 1)
-        #self.hide()
-        #return 1
+          #getApp().stopModal(self, 1)
+          #self.hide()
+          #return 1
 
         end
 
