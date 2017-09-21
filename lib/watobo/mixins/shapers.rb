b3768ed4309e277482dfdfe483631a958d32f695
Andreas Schmidt <a.schmidt@siberas.de>
2017-09-21 16:01:23 +0200

    fixed add_post_parm appending

--- lib/watobo/mixins/shapers.rb
+++ lib/watobo/mixins/shapers.rb
@@ -117,17 +117,23 @@ module Watobo #:nodoc: all
         end
 
         def add_post_parm(parm, value)
-          line = self.last
-          return false if line !~ /=/
-          line += "&#{parm}=#{value}"
-          self.pop
+          unless self.has_body?
+            # if we don't have a body we have to create one
+              line = ''
+              self.push "\r\n"
+            else
+              line = self.pop
+          end
+          line << '&' unless line.empty?
+          line << "#{parm}=#{value}"
+
           self.push line
         end
 
         def add_get_parm(parm, value)
           line = self.shift
           new_p = "&"
-          new_p = "?" if not self.element =~ /\?/
+          new_p = "?" unless line =~ /\?/
           new_p += parm
           line.gsub!(/( HTTP\/.*)/, "#{new_p}=#{value}\\1")
           self.unshift(line)
